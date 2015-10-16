fb31aa0a218366473ffd6dd59d7a77bf7f149849
Nater Kane <nater@naterkane.com>
2015-10-16 16:15:35 -0400

    add tmp db ref to mongodb sandbox

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/meanbase-dev'
+    uri: 'mongodb://tiaavisiondev:d3vt34m@ds039674.mongolab.com:39674/meanbase-dev'
   },
 
   seedDB: true
